41171ad78fcff96ace8d5ddfea2d1e9167e7249a
ShruthiBandi-27 <shruthi27998@gmail.com>
2023-05-19 19:26:06 +0530

    added login and signup

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,23 +1,56 @@
 import React, { useState } from 'react'
 import { Visibility, VisibilityOff } from "@mui/icons-material";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
 import { Button, FormControl, FormGroup, IconButton, Input, InputAdornment, InputLabel, TextField } from "@mui/material";
+import axios from 'axios';
+import {API} from '../global.js';
+import { useNavigate } from "react-router-dom";
+import { CircularProgress } from '@mui/material';
 
 const Login = () => {
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
     const [showPassword, setShowPassword] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const nav = useNavigate();
   
     
     const changeVisiblity = () => {
       setShowPassword(!showPassword);
     };
 
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      // Perform form submission logic
+    const handleSubmit = async (event) => {
+      setLoading(true);
+      if(!email || !password){
+        toast.error("Please fill all the fields");
+        setLoading(false);
+        return;
+      }
+
+      const headers = {
+        'Content-Type': 'application/json',
+      }
+  await axios.post(`${API}/api/user/login`,
+  {email, password},
+  {headers}
+  )
+  .then(res => {
+    toast.success("Login successful");
+    console.log(res.data)
+    localStorage.setItem('userInfo', JSON.stringify(res.data));
+    nav("/");
+    setLoading(false);
+  })
+  .catch(err => {
+    console.log(err);
+    toast.error(`Error: ${err}`);
+    setLoading(false);
+  })
     };
     return (
         <div className="form">
+        <ToastContainer />
           <form onSubmit={handleSubmit}>
             <FormGroup>
               <FormControl sx={{ mb: 2 }}>
@@ -59,11 +92,11 @@ const Login = () => {
              color="primary" 
              onClick={handleSubmit}
              sx={{width: "100%",mb: 2}}
+             disabled={loading}
              >
-              Login
+              {loading ? <CircularProgress size={24} /> : 'Login'}
             </Button>
             <Button
-            type="submit" 
             variant="contained"
              color="error" 
              onClick={ () => {
